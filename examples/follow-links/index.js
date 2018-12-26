082a868fca281803c00ed2fb8790e6c5095a11e8
Stephen Mizell <smizell@gmail.com>
2018-12-26 13:02:01 -0600

    Clarity for index.js file

--- examples/follow-links/index.js
+++ examples/follow-links/index.js
@@ -1,13 +1,14 @@
 const { queries } = require('moveablejson');
-
-
-const example = process.argv[2] || 'example1';
 const apiUrl = 'https://moveablejsonapi.glitch.me';
 
-// Same query for every example
+// Allow for changing examples through command line
+const example = process.argv[2] || 'example1';
+
+// Same query for every example to show it doesn't break
 const query = ['order', 'order_number'];
 
 async function api() {
+  // All examples are in the same API, so we just prepend that to the query
   const results = queries.followLink(apiUrl, [example, ...query]);
 
   for await (let result of results) {
