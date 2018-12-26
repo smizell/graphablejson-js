25660108a0f170719879ec5bc5900c296b62ff5a
Stephen Mizell <smizell@gmail.com>
2018-12-25 22:12:33 -0600

    Refactor and improve

--- queries.js
+++ queries.js
@@ -1,46 +1,45 @@
 const axios = require('axios');
 const _ = require('lodash');
 
-exports.raw = raw = async function* raw({ document, query }) {
-  // TODO: test for part undefined
-  // TODO: throw on everything except object
-
+exports.raw = raw = async function* raw({ document, query = [] }) {
   if (_.isPlainObject(document)) {
     const [key, ...restQuery] = query;
 
-    // Handle included collections
+    // Collections are denoted by objects that have an `$item` property.
     if ('$item' in document) {
-      yield* await raw({
+      yield* raw({
         document: document.$item,
         query
       });
 
       // Follow paginated links
       if (hasLink(document, 'next')) {
-        yield* await followLink(document, 'next', query);
+        yield* followLink(document, 'next', query);
       }
     }
 
+    // Like normal properties, the `$item` for collection can be a link or an
+    // array of links.
     else if (hasLink(document, '$item')) {
-      yield* await followLink(document, '$item', query);
+      yield* followLink(document, '$item', query);
 
       // Follow paginated links
       if (hasLink(document, 'next')) {
-        yield* await followLink(document, 'next', query);
+        yield* followLink(document, 'next', query);
       }
     }
 
     // Handle direct properties
     else if (key in document) {
-      yield* await raw({
+      yield* raw({
         document: document[key],
         query: restQuery
       })
     }
 
-    // Handle linked properties
+    // If a properties isn't found, we can look for a link with the same name.
     else if (hasLink(document, key)) {
-      yield* await followLink(document, key, restQuery);
+      yield* followLink(document, key, restQuery);
     }
   }
 
@@ -53,6 +52,7 @@ exports.raw = raw = async function* raw({ document, query }) {
     }
   }
 
+  // TODO: only yield on strings, numbers, and booleans
   else {
     yield document;
   }
@@ -69,21 +69,14 @@ function getLinkName(document, key) {
 async function* followLink(document, key, query) {
   // TODO: needs error handling
   const linkName = getLinkName(document, key);
-  const link = document[linkName];
+  const urlProp = document[linkName];
+  const urls = raw({ document: urlProp });
 
-  if (_.isArray(link)) {
-    for (let item of link) {
-      let resp = await axios.get(item);
-      yield* await raw({
-        document: resp.data,
-        query
-      });
-    }
-  }
-
-  else {
-    let resp = await axios.get(link);
-    yield* await raw({
+  // This allows us to handle one or many URLs. We can let the `raw` function
+  // return a generator so that we can always loop over the URLs no matter what.
+  for await (let url of urls) {
+    let resp = await axios.get(url);
+    yield* raw({
       document: resp.data,
       query
     });
